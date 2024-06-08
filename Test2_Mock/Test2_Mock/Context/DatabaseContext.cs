658655ea4b6f65aad780aa1f761ef9813d037355
z1olkowsk1 <janaziolkowski@gmail.com>
2024-06-08 16:02:13 +0200

    I have added 2 constructors to the Context and needed method OnModelCreating for further configuration of entities.

--- Test2_Mock/Test2_Mock/Context/DatabaseContext.cs
+++ Test2_Mock/Test2_Mock/Context/DatabaseContext.cs
@@ -1,6 +1,20 @@
-﻿namespace Test2_Mock.Context;
+﻿using Microsoft.EntityFrameworkCore;
 
-public class DatabaseContext
+namespace Test2_Mock.Context;
+
+public class DatabaseContext : DbContext
 {
-    
+    //Created two constructor needed for database
+    protected DatabaseContext()
+    {
+    }
+
+    public DatabaseContext(DbContextOptions options) : base(options)
+    {
+    }
+    //TODO: configure here adding all configurations for entities!!
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
 }
